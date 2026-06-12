eda2ca5e976e9489139f8b66b8d2a15eeed8393c
libingjiang47 <libingjiang47@gmail.com>
2026-06-12 03:14:37 +0000

    Update Chinese docs and simplify home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import HomeHero from '@site/src/components/HomeHero';
 import QuickStartCards from '@site/src/components/QuickStartCards';
-import ProtocolFlow from '@site/src/components/ProtocolFlow';
-import SystemComponents from '@site/src/components/SystemComponents';
 
 export default function Home(): React.ReactElement {
   return (
@@ -15,8 +13,6 @@ export default function Home(): React.ReactElement {
         <HomeHero />
         <div className="container">
           <QuickStartCards />
-          <ProtocolFlow />
-          <SystemComponents />
         </div>
       </main>
     </Layout>
